bc2af68717ec3f3b64b95c13dfa5ba670a50d21c
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-09-06 00:24:43 +0900

    [first commit]

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+
+      <Header />
+
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <Footer />
+
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
