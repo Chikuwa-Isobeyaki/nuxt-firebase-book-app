0486ebf6cda417373b648bea5fbc0a31b10d24e9
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-09-07 19:02:02 +0900

    [Add&Test]dotenvをインストール firebaseにデータを送信

---
+++ pages/firebasetest/addData.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-btn @click="addData">追加</v-btn>
+  </div>
+</template>
+
+<script>
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+export default {
+  data(){
+    return {
+      id: '001',
+      title: 'テスト'
+    }
+  },
+  methods: {
+    addTest(){
+      console.log("test")// eslint-disable-line no-console
+    },
+    async addData(){
+      try {
+        const db = getFirestore(this.$firebase)
+        const docRef = await addDoc(collection(db, 'tasks'), {
+          id: this.id,
+          title: this.title
+        })
+        console.log('追加したデータのID:', docRef.id) // eslint-disable-line no-console
+      } catch(e) {
+        console.error('error:', e) // eslint-disable-line no-console
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -13,10 +9,19 @@ const firebaseConfig = {
   appId: process.env.APP_ID
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCu7kw_vB9zxBNpeuI97TsJcy1IeWG8Ta4",
+//   authDomain: "nuxt-spa-book-app-90ce5.firebaseapp.com",
+//   projectId: "nuxt-spa-book-app-90ce5",
+//   storageBucket: "nuxt-spa-book-app-90ce5.appspot.com",
+//   messagingSenderId: "532494128702",
+//   appId: "1:532494128702:web:30e5c81fd594f24742a36f"
+// };
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-//firebaseを他のファイルで使えるよう設定
+// firebaseを他のファイルで使えるよう設定
 export default ( context, inject) => {
   inject('firebase', firebaseApp)
 }
